40205058faf4c8b603fe530e25eb4ebb4741d070
DavideBiondi <161145387+DavideBiondi@users.noreply.github.com>
2025-08-16 13:18:15 +0000

    Added Flask templating syntax elements and jsonify, to create a dynamic page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 #An App is an object of Flask class. It is our WSGI application. WSGI stands for Web Server Gateway Interface.
 #In any python script, you have this variable __name__ which is a built-in variable.
@@ -12,7 +12,23 @@ And the domain name is www.example.com.
 The symbol '@' is a decorator. A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure. Decorators are usually called before the definition of a function you want to decorate."""
 
 """It is not a good practice to list all the jobs in the HTML file. Instead, we should list them in the python file and then pass them to the HTML file. And then is sent to the browser as a response we can see on the screen.
-So we simulated a database with a list of dictionaries. Each dictionary represents a job. And then we pass this list to the HTML file. And then we can use this list in the HTML file.
+So we simulated a database with a list of dictionaries. Each dictionary represents a job. And then we pass this list to the HTML file. And then we can use this list in the HTML file, using Flask templating syntax.
+"""
+"""Another way to return dynamic data - besides Flask templating syntax - is to use the jsonify function. This function converts a dictionary into a JSON object. JSON stands for JavaScript Object Notation. It is a lightweight data-interchange format. It is easy for humans to read and write and easy for machines to parse and generate.
+The JSON format usage is strictly linked to the concept of RESTful APIs. REST stands for Representational State Transfer. It is an architectural style for designing networked applications. It relies on a stateless, client-server, cacheable communications protocol -- the HTTP. 
+RESTful means that the API follows the REST principles.
+The difference between REST API and RESTful API is that REST API is a set of rules and principles that define how a client and server should communicate, while RESTful API is an API that follows these rules and principles.
+RESTful APIs are used to exchange data between a client and a server. The client sends a request to the server and the server sends a response back to the client. The response is usually in the form of a JSON object. So instead of returning HTML, we can return JSON. And then the client can use this JSON to display the data in the browser. 
+
+With the function list_jobs, we used jsonify to return a JSON object. The jsonify function converts a dictionary into a JSON object. The dictionary is the list of jobs. The list of jobs is a list of dictionaries. Each dictionary represents a job. We registered a new route /jobs, in which we return the list of jobs as a JSON object. When people say rest API or JSON API, or API endpoint, they are talking about a URL that returns data in the form of a JSON object.
+
+So the difference between creating an HTML endpoint and a JSON endpoint is that the HTML endpoint returns HTML and the JSON endpoint returns JSON. The HTML endpoint is used to display data in the browser and the JSON endpoint is used to exchange data between a client and a server.
+
+In order to differentiate between the two, often a route starting with /api is used for JSON endpoints. For example, /api/jobs. This is a convention. It is not a rule. So we can use /jobs for HTML endpoints and /api/jobs for JSON
+
+API stands for Application Programming Interface. It is a set of rules and protocols that allows different software applications to communicate with each other. ABI stands for Application Binary Interface. The difference between API and ABI is that API is a set of rules and protocols that allows different software applications to communicate with each other, while ABI is a set of rules and protocols that allows different software applications to communicate with each other at the binary level.
+
+
 """
 
 JOBS = [
@@ -39,16 +55,27 @@ JOBS = [
         'title': 'Backend engineer',
         'location': 'San Francisco, USA',
         'salary': '$ 120,000'
+    },
+    {
+        'id': 5,
+        'title': 'DevOps engineer',
+        'location': 'Chicago, USA'
     }
 ]
 
 @app.route("/")
 #The "/" is the root URL. The root URL is the URL of the home page of a website.
-def hello_world():
+#def hello_world():
+def hello_jovian():
     #return "<p>Hello, World!</p>"
     #We send the list of jobs to the HTML file by passing JOBS as an argument to the render_template function. The argument is JOBS but the parameter name could have been anything.
-    return render_template('home.html', jobs=JOBS)
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
 print(__name__)
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
+    
 """Instead of using flusk run command, we can use app.run command to run the application:"""
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
